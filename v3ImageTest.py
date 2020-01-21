81ff0685784c36f12e0f2a8ed86a88dd67299d59
namiwa <khairul.nasri895@gmail.com>
2020-01-21 14:30:06 +0800

    feat(image): added image testing with different versions of yolo

---
+++ v3ImageTest.py
@@ -0,0 +1,102 @@
+from __future__ import print_function
+import numpy as np
+import time
+import cv2
+import os
+
+
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.abspath("yolov3-tiny/coco.names")
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.realpath('yolov3-tiny/yolov3-tiny.weights')
+configPath = os.path.realpath("yolov3-tiny/yolov3-tiny.cfg")
+ 
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+while True:
+  
+  img = cv2.imread("testData/top-down-small.jpg")
+
+  
+
+  (H, W) = img.shape[:2]
+
+
+  ln = net.getLayerNames()
+  ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+  blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416),
+	swapRB=True, crop=False)
+  net.setInput(blob)
+  layerOutputs = net.forward(ln)
+
+  boxes = []
+  confidences = []
+  classIDs = []
+
+  for output in layerOutputs:
+	# loop over each of the detections
+    for detection in output:
+      # extract the class ID and confidence (i.e., probability) of
+      # the current object detection
+      scores = detection[5:]
+      classID = np.argmax(scores)
+      confidence = scores[classID]
+  
+      # filter out weak predictions by ensuring the detected
+      # probability is greater than the minimum probability
+      if confidence > 0:
+        # scale the bounding box coordinates back relative to the
+        # size of the image, keeping in mind that YOLO actually
+        # returns the center (x, y)-coordinates of the bounding
+        # box followed by the boxes' width and height
+        box = detection[0:4] * np.array([W, H, W, H])
+        (centerX, centerY, width, height) = box.astype("int")
+  
+        # use the center (x, y)-coordinates to derive the top and
+        # and left corner of the bounding box
+        x = int(centerX - (width / 2))
+        y = int(centerY - (height / 2))
+  
+        # update our list of bounding box coordinates, confidences,
+        # and class IDs
+        boxes.append([x, y, int(width), int(height)])
+        confidences.append(float(confidence))
+        classIDs.append(classID)
+
+  idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0, 50)
+  people_count = 0
+  if len(idxs) > 0:
+	# loop over the indexes we are keeping
+    for i in idxs.flatten():
+      # extract the bounding box coordinates
+      (x, y) = (boxes[i][0], boxes[i][1])
+      (w, h) = (boxes[i][2], boxes[i][3])
+  
+      # draw a bounding box rectangle and label on the image
+      color = [int(c) for c in COLORS[classIDs[i]]]
+      cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+      text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+      if LABELS[classIDs[i]] == "person":
+        people_count += 1
+
+      cv2.putText(img, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+  
+# show the output image
+  cv2.imshow("Image", img)
+  print(people_count)
+
+  ch = cv2.waitKey(1)
+  if ch == 27:
+      break
+
